8b91709f671948f7402f71aa669970079c19db73
hanifmaleki <hanif_1362@yahoo.com>
2024-03-27 00:04:32 +0100

    Add Form CommentType

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,28 +3,56 @@
 namespace App\Controller;
 
 use App\Entity\Conference;
+use App\Entity\Comment;
+use App\Form\CommentType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ConferenceController extends AbstractController
 {
+
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+    ) {
+    }
+
     #[Route('/', name: 'homepage')]
-    public function index(Conference $conference, ConferenceRepository $conferenceRepository): Response
-    {
+    public function index(
+        Request $request,
+        Conference $conference,
+        ConferenceRepository $conferenceRepository,
+        CommentRepository $commentRepository,
+        // #[Autowire('%photo_dir%')] string $photoDir,
+    ): Response {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+            if ($photo = $form['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)) . '.' . $photo->guessExtension();
+                // $photo->move($photoDir, $filename);
+                $comment->setPhotoFilename($filename);
+            }
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['id' => $conference->getId()]);
+        }
+
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
         return $this->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
         ]);
     }
-
-    /* #[Route('/homepage', name: 'homepage')]
-    public function index2(Conference $conference, ConferenceRepository $conferenceRepository): Response
-    {
-        return $this->index($conference, $conferenceRepository);
-    } */
 
     #[Route('/conference/{id}', name: 'conference')]
     public function show(
@@ -33,6 +61,9 @@ class ConferenceController extends AbstractController
         CommentRepository $commentRepository,
         ConferenceRepository $conferenceRepository
     ): Response {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
         return $this->render('conference/show.html.twig', [
@@ -41,6 +72,7 @@ class ConferenceController extends AbstractController
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $form,
         ]);
     }
 }
